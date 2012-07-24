1bcc30e321bf32bff8d4cba446b35701da548109
Erik Knutsson <erik@schimpanz.com>
2012-07-24 11:45:35 +0200

    add main.js

---
+++ main.js
@@ -0,0 +1,32 @@
+/* #CREATE DROPDOWN MENU
+================================================== */
+$(document).ready(function () {
+    // Create the dropdown base
+    $("<select />").appendTo(".mobile-navigation");
+
+    // Create default option "Go to..."
+    $("<option />", {
+        "selected": "selected",
+        "value": "",
+        "text": "-- Navigera"
+    }).appendTo(".mobile-navigation select");
+
+    // Populate dropdown with menu items
+    $(".main-nav ul li a").each(function () {
+        var el = $(this);
+        $("<option />", {
+            "value": el.attr("href"),
+            "text": el.text()
+        }).appendTo(".mobile-navigation select");
+    });
+
+    $(".mobile-navigation select").change(function () {
+        window.location = $(this).find("option:selected").val();
+    });
+});
+
+/* #CLONE ELEMENTS FOR MOBILE
+================================================== */
+$(document).ready(function () {
+    $(".clone-to-top").clone().appendTo(".mobile-top");
+});
